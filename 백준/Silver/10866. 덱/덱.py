ffc3f58b373f8946b37514cc16c28fe0924cf101
Connie <102277646+Conni2@users.noreply.github.com>
2023-11-17 00:11:52 +0900

    [Silver IV] Title: 덱, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,36 @@
+import sys
+n = int(sys.stdin.readline())
+stack = []
+for i in range (n):
+    order = sys.stdin.readline().split()
+    if order[0] == 'push_front':
+        stack.insert(0, int(order[1]))
+    elif order[0] == 'push_back':
+        stack.append(int(order[1]))
+    elif order[0] == 'pop_front':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop(0))
+    elif order[0] == 'pop_back':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop(-1))
+    elif order[0] == 'size':
+        print(len(stack))
+    elif order[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif order[0] == 'front':
+        if len(stack) ==0:
+            print(-1)
+        else:
+            print(stack[0])
+    elif order[0] == 'back':
+        if len(stack) ==0:
+            print(-1)
+        else:
+            print(stack[-1])
